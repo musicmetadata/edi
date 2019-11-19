e75c4cf58ceec5bf7b75f24e1ecef137a6aaafb9
matija <matijakolaric@users.noreply.github.com>
2019-11-19 16:10:12 +0100

    header calculations

--- music_metadata/edi/file.py
+++ music_metadata/edi/file.py
@@ -4,3 +4,14 @@ import io
 class EDIFile(io.TextIOWrapper):
     encoding = 'latin1'
 
+    def __init__(self):
+        super().__init__()
+        self._header = self.get_header()
+
+    def get_header(self):
+        if hasattr(self, '_header'):
+            return self._header
+        position = self.tell()
+        header = self.readline()
+        self.seek(position)
+        return header
