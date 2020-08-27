3d59a004a23c19b811b0ee83f2fe70c481d44422
matijakolaric <matijakolaric@users.noreply.github.com>
2020-08-27 18:06:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="music_metadata_edi",
-    version="19",
+    version="20.8",
     author="Matija Kolarić",
     author_email="matijakolaric@users.noreply.github.com",
     description="Music Metedata - EDI-based formats",
