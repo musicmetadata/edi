6ae6de22a7485a3bb4fa069fcb3f642eb3163bd3
matijakolaric <matijakolaric@users.noreply.github.com>
2020-10-13 15:01:16 +0200

    bumped version to beta 4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="music_metadata_edi",
-    version="20.9b3",
+    version="20.9b4",
     author="Matija Kolarić",
     author_email="matijakolaric@users.noreply.github.com",
     description="Music Metedata - EDI-based formats",
